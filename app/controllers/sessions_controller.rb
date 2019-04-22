436b7ac9517d1f9625be8d63427219296e842803
Tadea <tsimunoviczg@gmail.com>
2019-04-22 14:35:57 -0400

    add Login for user

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,9 +1,19 @@
 class SessionsController < ApplicationController
   def new
+    @user = User.new
+    render :login
   end
 
   def create
-  end
+       @user = User.find_by(email: params[:email])
+       if @user  && @user.authenticate(params[:password])
+        session[:user_id] = @user.id
+        redirect_to user_path(@user)
+       else
+        flash[:message] = "Something went wrong!"
+        redirect_to root_path
+       end
+    end
 
   def home
   end
