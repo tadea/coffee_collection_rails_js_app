d232da87e518f14b1296fb36ab56f796b664761b
Tadea <tsimunoviczg@gmail.com>
2019-04-23 11:49:24 -0400

    edit routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,20 @@
 Rails.application.routes.draw do
-  
+
   root 'sessions#home'
-  get '/signup' => 'users#new'
-  get '/login' => 'sessions#new'
-  post '/login' => 'sessions#create'
-  delete '/logout' => 'sessions#destroy'
-  resources :users
-  resources :origins
-  resources :coffees
+
+
+  resources :users, only: [:new, :create, :show] do
+    resources :coffees, only: [:index]
+  end
+
+  resources :coffees do
+    resources :reviews, only: [:create]
+  end
+
+  resources :sessions, only: [:new, :create, :destroy]
+
+
+  #resources :origins
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
