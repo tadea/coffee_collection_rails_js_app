72dcd45102d24d9ac11ec4181539b4c1f8b5b4d3
Tadea <tsimunoviczg@gmail.com>
2019-04-28 14:13:31 -0400

    add controller for Reviews

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,31 @@
 class ReviewsController < ApplicationController
+
+  def index
+  end
+
+  def new
+
+  end
+
+  def show
+  end
+
+  def create
+    
+  end
+
+def edit
+end
+
+def update
+end
+
+def destroy
+end
+private
+
+def review_params
+  params.require(:review).permit(:comment)
+end
+
 end
