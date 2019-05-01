cc32d0fcc3c77595d7e9db745ad23c4e8e76c72b
Tadea <tsimunoviczg@gmail.com>
2019-05-01 11:43:16 -0400

    add associations for Coffee and User model

--- app/models/coffee.rb
+++ app/models/coffee.rb
@@ -1,10 +1,8 @@
 class Coffee < ApplicationRecord
-  has_many :reviews
-  has_many :users, through: :reviews
-  #belongs_to :origin, optional: true
+  belongs_to :user
+  belongs_to :grind
+  #has_many :reviews
 
-  scope :french_press, -> {where(grind: "French Press")}
-  scope :esspreso, -> {where(grind: "Esspreso")}
-  scope :pour_over, -> {where(grind: "Pour Over")}
-  scope :turkish_coffee, -> {where(grind: "Turkish Coffee")}
+
+  
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,10 @@
 class User < ApplicationRecord
-  has_secure_password
 
+  has_many :coffes
   has_many :reviews
-  has_many :coffees, through: :reviews
+  #has_many :coffees, through: :reviews
+  has_secure_password
+  validates :email, presence: true, uniqueness: {case_sensitive: :false}
 
-  validates :email, :password, :presence => true
 
 end
