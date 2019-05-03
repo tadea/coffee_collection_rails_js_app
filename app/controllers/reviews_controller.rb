adb6c07cb42f1af7d645779eb548b4a4dd335918
Tadea <tsimunoviczg@gmail.com>
2019-05-03 09:52:04 -0400

    add CRUD for controller

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,31 +1,50 @@
 class ReviewsController < ApplicationController
-
-  def index
-  end
+    before_action :find_coffee
+    before_action :find_review, only: [ :edit, :update, :destroy]
 
   def new
-
-  end
-
-  def show
+    @review = Review.new
   end
 
   def create
-    
+    @review = Review.new(review_params)
+    @review.coffee_id = @coffee.id
+    @review.user_id = current_user.id
+      if @review.save
+        redirect_to coffee_path(@coffee)
+      else
+        render :new
+      end
   end
+
 
 def edit
 end
 
 def update
+  if @review.update(review_params)
+    redirect_to coffee_path(@coffee)
+  else
+    render 'edit'
+  end
 end
 
 def destroy
+  @review.destroy
+  redirect_to coffees_path
 end
-private
-
-def review_params
-  params.require(:review).permit(:comment)
 end
 
-end
+  private
+
+  def review_params
+    params.require(:review).permit(:comment)
+  end
+
+  def find_coffee
+    @coffee = Coffee.find(params[:coffee_id])
+  end
+
+  def find_review
+    @review = Review.find(params[:id])
+  end
