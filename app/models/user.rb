cbd04ec998aa124f71d4790c068d475f32b5e3d3
Tadea <tsimunoviczg@gmail.com>
2019-05-03 09:48:02 -0400

    add validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,6 @@
 class User < ApplicationRecord
-
-  has_many :coffes
   has_many :reviews
-  #has_many :coffees, through: :reviews
+  has_many :coffees, through: :reviews
   has_secure_password
   validates :email, presence: true, uniqueness: {case_sensitive: :false}
-
-
 end
