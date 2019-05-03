8ff68964fbfe0777f8be2540495c9218d56a41f1
Tadea <tsimunoviczg@gmail.com>
2019-05-03 09:47:46 -0400

    add validation

--- app/models/coffee.rb
+++ app/models/coffee.rb
@@ -1,8 +1,7 @@
 class Coffee < ApplicationRecord
-  belongs_to :user
   belongs_to :grind
-  #has_many :reviews
+  has_many :reviews
+  has_many :users, through: :reviews
 
-
-  
+  validates :name, :description, :process, :origin, :grind,  presence: true
 end
