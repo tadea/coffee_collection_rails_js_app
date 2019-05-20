2b0cccebec8642b9dfc92180fc431b91648df053
Tadea <tsimunoviczg@gmail.com>
2019-05-20 09:57:47 -0400

    add validation for image

--- app/models/coffee.rb
+++ app/models/coffee.rb
@@ -5,11 +5,20 @@ class Coffee < ApplicationRecord
 
   has_one_attached :image
 
-  validates :name, :description, :process, :origin, :grind, :image,  presence: true
-
+  validates :name, :description, :process, :origin,  presence: true
+  validates :image, presence: true
+  validate :image_type
 
 
   scope :coffee_origin, -> { order('origin')}
   scope :reviewed,      -> { joins(:reviews).distinct("reviews.coffee_id") }
 
-end
+
+  private
+
+  def image_type
+    if image.attached? == false
+      errors.add(:image, "must have an attachement.")
+    end
+    end
+  end
